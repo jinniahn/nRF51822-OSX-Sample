0716b3cc9574c7ebb0a0e88362a0444d13dcffc4
Jinsub Ahn <jinniahn@magice.co>
2015-03-18 12:48:43 +0900

    init: copy from https://github.com/Sproutling/nRF51822-OSX-Sample

---
+++ tools/OSX/arm-cs-tools/lib/gcc/arm-none-eabi/4.7.3/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../../gcc-4.7-2013.05/configure --prefix=/Users/jinni/arm-cs-tools --with-pkgversion='32-bit ARM EABI Toolchain JBS-2013.05-23-v2011.09-69-41-gd36d291' --with-bugurl=https://github.com/jsnyder/arm-eabi-toolchain --target=arm-none-eabi --enable-languages=c,c++ --with-gnu-ld --with-gnu-as --with-newlib --disable-nls --disable-libssp --disable-shared --enable-threads --with-headers=yes --disable-libmudflap --disable-libgomp --enable-lto --disable-libstdcxx-pch --enable-poison-system-directories --with-sysroot=/Users/jinni/arm-cs-tools/arm-none-eabi --with-build-time-tools=/Users/jinni/arm-cs-tools/arm-none-eabi/bin --enable-extra-sgxxlite-multilibs --with-specs='%{save-temps: -fverbose-asm} -D__CS_SOURCERYGXX_MAJ__=2013 -D__CS_SOURCERYGXX_MIN__=05 -D__CS_SOURCERYGXX_REV__=23 %{O2:%{!fno-remove-local-statics: -fremove-local-statics}} %{O*:%{O|O0|O1|O2|Os:;:%{!fno-remove-local-statics: -fremove-local-statics}}}' --with-gmp=/Users/jinni/Documents/nf51/arm-eabi-toolchain//libs/ --with-mpfr=/Users/jinni/Documents/nf51/arm-eabi-toolchain//libs/ --with-mpc=/Users/jinni/Documents/nf51/arm-eabi-toolchain//libs/";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
